7249e0dd2bf2d2237fb928ff2cbfe385359fcc20
minji <ddaa637878@gmail.com>
2025-06-16 15:20:48 +0900

    modify: 로그인/회원가입 가로 폭 수정

--- src/app/login/layout.tsx
+++
@@ -1,9 +0,0 @@
-import type { ReactNode } from 'react';
-
-export default function LoginLayout({ children }: { children: ReactNode }) {
-  return (
-    <div className="bg-gray-100 h-screen flex items-center justify-center">
-      {children}
-    </div>
-  );
-}
