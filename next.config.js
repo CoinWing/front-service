717144e86b1a489e75f2fc023ddcef422b155ada
JaeHwan Kim <iwantbaobab@gmail.com>
2025-08-25 18:13:01 +0900

    Hotfix: 프로덕션 환경 console log 비활성화

--- next.config.js
+++ next.config.js
@@ -6,10 +6,10 @@ const nextConfig = {
     WS_URL: process.env.WS_URL || 'wss://api.upbit.com/websocket/v1',
     UPBIT_API_URL: process.env.UPBIT_API_URL || 'https://api.upbit.com/v1',
   },
-  // compiler: {
-  //   // 프로덕션 빌드에서 console.log 제거
-  //   removeConsole: process.env.NODE_ENV === 'production',
-  // },
+  compiler: {
+    // 프로덕션 빌드에서 console.log 제거
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
 }
 
 module.exports = nextConfig 
